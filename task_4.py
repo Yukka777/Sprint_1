cf1bbf28972ae32b94bf09f3b0c942c1ced4806e
Yukka777 <109177551+Yukka777@users.noreply.github.com>
2025-05-21 23:50:01 +0300

    Create task_4.py

---
+++ task_4.py
@@ -0,0 +1,11 @@
+new_tasks = ['task_001', 'task_011', 'task_007', 'task_015', 'task_005']
+completed_tasks = ['task_002', 'task_012', 'task_006']
+
+# Перенос task_005 в completed_tasks
+completed_tasks.append(new_tasks.pop(new_tasks.index('task_005')))
+
+# Удаление task_007
+new_tasks.remove('task_007')
+
+# Вывод последней задачи
+print(new_tasks[-1])
