5406c86e4a19a4cba4cbc7bafb237471d92dd0e2
Yukka777 <109177551+Yukka777@users.noreply.github.com>
2025-05-22 21:17:33 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -3,7 +3,7 @@ import re
 time_string = '1h 45m,360s,25m,30m 120s,2h 60s'
 total_minutes = 0
 
-# Разделяем строку на отдельные временные значения
+# Разделяем строку на отдельные временные значения 
 time_values = re.split(r",|\s", time_string)
 
 for value in time_values:
